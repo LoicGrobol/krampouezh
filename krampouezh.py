794b3f011096d26e41dc2746573b6f519c227a62
Evpok <loic.grobol@gmail.com>
2014-12-27 18:54:11 +0100

    Reworked `krampouezh.polynomial` for more flexibility and adapted the est accordingly Added `krampouezh.interpol_tikz' and `krampouezh.piecewise_tikz` to generate pgf/tikz code to graph the cubic interpolants

--- krampouezh.py
+++ krampouezh.py
@@ -29,19 +29,34 @@ def piecewise_geogebra(bounds, functions):
 					b[0],
 					b[-1])
 
-def polynomial(coefs, indeterminate='x', times=''):
+def polynomial(coefs, template='{0:+f}x^{1:d}', constant_template='{0}'):
   '''Return the representation of the polynomial with coefs `coefs`.
   
-    >>> polynomial((1,2,3), 'y', '×')
-    '1+2×y^2+3×y^3'
+    >>> polynomial((1,2,3))
+    '1+2x^2+3x^3'
+    >>> polynomial((1,2,3), {0:+f}×y^({3:f})'
+    '1+2×y^(1.0)+3×y^(2.0)'
   '''
-  return str(coefs[0]) + ''.join('{0:+f}{1}{2}^{3:d}'.format(c,t,i,p)
-			      for c,t,i,p in zip(coefs[1:],it.repeat(times), it.repeat(indeterminate), it.count(1)))
+  return ''.join(template.format(c,p) if p != 0 else constant_template.format(c) for c,p in zip(coefs,it.count()) if c != 0)
 
 def interpol_geogebra(points: '((x₀,y₀),(x₁,y₁),…)'):
+  '''Return a Geogebra definition of the interpolant of `points`.'''
   x,y = zip(*sorted(points))
   coefs = list(libinterpol.cubic_coefs(points))
-  functions = (polynomial(c,'(x{0:+d})'.format(-x0)) for c,x0 in zip(coefs,x))
+  functions = (polynomial(c,'{{0:+f}}*(x{0:+d})^{{1:d}}'.format(-x0)) for c,x0 in zip(coefs,x))
   return piecewise_geogebra(x,functions)
+
+def piecewise_tikz(bounds, functions):
+  '''Return TikZ code to display a function defined piecewise on intervals
+     delimited by `bounds` by `functions`.'''
+  b = sorted(bounds)
+  return '\n'.join(r'\draw[smooth,samples=100,domain={1}:{2}] plot(\x,{{{0}}});'.format(f,a,b) for f,a,b in zip(functions,b[:-1],b[1:]))
   
-print(interpol_geogebra(((0,-2),(1,1),(2,4),(6,-1),(-2,1))))
+def interpol_tikz(points: '((x₀,y₀),(x₁,y₁),…)'):
+  '''Return a TikZ definition of the interpolant of `points`.'''
+  x,y = zip(*sorted(points))
+  coefs = list(libinterpol.cubic_coefs(points))
+  functions = (polynomial(c,'{{0:+f}}*(\\x{0:+d})^({{1:d}})'.format(-x0)) for c,x0 in zip(coefs,x))
+  return piecewise_tikz(x,functions)
+  
+print(interpol_tikz(((0,1),(2,6),(3,-5),(7,0))))
